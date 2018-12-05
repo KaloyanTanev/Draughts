0ec1cc4ab26feebf56d1f55200f834e5af8b4bb7
Momchilski <45099649+Momchilski@users.noreply.github.com>
2018-12-05 05:35:18 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
+var players = {};
+
+
 app.set('port', 6969);
 app.use('/public', express.static(__dirname + '/public/'));
 
@@ -16,13 +19,39 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/gamescreen', function(request, response) {
+    response.sendFile(path.join(__dirname, 'gamescreen.html'));
+});
+
+
+io.on('connection', function (socketIO) {
+  console.log('a user connected');
+
+  // create a new player and add it to our players object
+players[socket.id] = {
+    rotation: 0,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'white' : 'red'
+  };
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+
+    // remove this player from our players object
+delete players[socket.id];
+// emit a message to all players to remove this player
+io.emit('disconnect', socket.id);
+  });
+});
+
 
 //Start server
 server.listen(6969, function(){
-console.log('hui');
+console.log('successful');
 });
 
-
-//Web4orapi handlers
-io.on('connection', function(socket){
-});
